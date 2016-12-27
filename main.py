b7afced58452495e39414dbde7f5310a3fed067c
Raymond Seger <raymondseger@live.com>
2016-12-27 23:15:43 +0700

    tuple data in python

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
-groceries = ['apple', 'mango']
-print(groceries)
-print(groceries.count('apple'))
+tuple_example = ('apple', 'mango') # tuple cannot be changed or added
+print(tuple_example)
+
+# so we need to change it to list to add / change the data
+list_object = list(tuple_example)
+list_object.append('orange')
+print(list_object)
+
+#can make the list to tuple again
+tuple_object_2 = tuple(list_object)
+print(tuple_object_2)
