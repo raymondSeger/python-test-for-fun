fec89e62db53a0e1017a767476ebcf9b83387489
Raymond Seger <raymondseger@live.com>
2017-01-01 12:24:08 +0700

    HEREDOC

--- main.py
+++ main.py
@@ -1,17 +1,4 @@
-collection = {
-    "name"  : "raymond",
-    "age"   : 25
-}
-
-print(collection)
-
-words = {}
-words["key1"] = "Raymond"
-words["key2"] = "Jett"
-
-print(words)
-
-collections2 = {}
-collections2[0] = 124
-
-print(collections2)
+print("""
+hi all
+how are you
+""")
