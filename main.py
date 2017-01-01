7a706cbe7ed59645e5cc5f5576bd804416a1f03d
Raymond Seger <raymondseger@live.com>
2017-01-01 14:00:55 +0700

    create set from a list

--- main.py
+++ main.py
@@ -1,7 +1,2 @@
-tuple_example = (1, 2, 3)
-x, y, z = tuple_example
 
-print(tuple_example) # (1, 2, 3)
-print(x) #1
-print(y) #2
-print(z) #3
+print( set( ['apple', 'orange', 'apple', 'pear', 'orange', 'banana'] ) ) #{'orange', 'banana', 'pear', 'apple'}
