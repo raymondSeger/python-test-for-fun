82153e720945de3c30b4e832efabe5bfab7b31e0
Raymond Seger <raymondseger@live.com>
2017-01-01 12:58:41 +0700

    string and list indexing

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
-print( 3 * 'un' + 'ium' ) # unununium
+word = 'Python'
+
+print( word [0] )  # P
+print( word [5] ) #n
+print( word [0:] ) #Python
+print( word [0:6] ) #Python
+print( word [0:5] ) #Pytho
+
+print( word [-1] ) #n
+print( word [-2] ) #o
+
+print( word [:3] ) #Pyt
+print( word [3:] ) #hon
+print( word [:3] + word [3:] ) #Python
+
+print( word [:] ) #Python
