d04935f149ab019ad7c4bd559fbf70eb4404b993
Raymond Seger <raymondseger@live.com>
2017-01-15 13:38:01 +0700

    keyboard events

--- main.py
+++ main.py
@@ -12,8 +12,23 @@ class HelloApp:
         self.button = ttk.Button(master, text="Click Me", command=self.button_handler)
         self.button.pack()
 
+        master.bind("<KeyPress>", self.key_press_handler)
+
+        master.bind("<Control-c>", self.control_c_handler)
+
     def button_handler(self):
         print("hi all")
+
+    def key_press_handler(self, event):
+        print(event.type)
+        print(event.widget)
+        print(event.char)
+        print(event.keysym)
+        print(event.keycode)
+
+    def control_c_handler(self, event):
+        print('control c pressed')
+
 
 
 def main():
