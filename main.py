9d8d09e430ce20f55b5511f07f88029b9089c4cb
Raymond Seger <raymondseger@live.com>
2017-01-15 12:13:08 +0700

    canvas, you won't use this that much

--- main.py
+++ main.py
@@ -6,20 +6,10 @@ from tkinter import ttk
 class HelloApp:
 
     def __init__(self, master):
-        self.treeview = ttk.Treeview(master)
-        self.treeview.pack()
-
-        self.treeview.insert('', '0', 'item1', text="First Item")
-        self.treeview.insert('', '1', 'item2', text="Second Item")
-        self.treeview.insert('', 'end', 'item3', text="Third Item")
-
-        self.logo = PhotoImage(file="a634dc.png").subsample(100,100)
-        # insert this treeview inside item2, which is in position 1
-        self.treeview.insert('item2', 'end', 'python', text="Python", image=self.logo)
-        self.treeview.item('item2', open=True) #open by default
-
-        self.treeview.move('item1', 'item2', 'end') #put item 1 to item2, and puts it at the end index
-
+        self.canvas = Canvas(master)
+        self.canvas.pack()
+        self.canvas.config(width=640, height=480)
+        self.canvas.create_line(160, 360, 480, 120, fill="blue", width=5)
 
 def main():
     root = Tk()
