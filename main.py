60ac78b75c7674ec02d7adead0816b207680ff10
Raymond Seger <raymondseger@live.com>
2017-01-15 10:13:53 +0700

    simple helloWorld TkInter

--- main.py
+++ main.py
@@ -1,23 +1,6 @@
 import tkinter
 
-class Application(tkinter.Frame):
-    def __init__(self, master=None):
-        super().__init__(master)
-        self.pack()
-        self.create_widgets()
-
-    def create_widgets(self):
-        self.hi_there = tkinter.Button(self)
-        self.hi_there["text"] = "Hello World\n(click me)"
-        self.hi_there["command"] = self.say_hi
-        self.hi_there.pack(side="top")
-
-        self.quit = tkinter.Button(self, text="QUIT", fg="red", command=root.destroy)
-        self.quit.pack(side="bottom")
-
-    def say_hi(self):
-        print("hi there, everyone!")
-
 root = tkinter.Tk()
-app = Application(master=root)
-app.mainloop()
+tkinter.Label(root, text="hello world").pack()
+root.mainloop()
+
