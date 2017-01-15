646b474d2c67989d42542a6b50c6807662d6da24
Raymond Seger <raymondseger@live.com>
2017-01-15 10:24:41 +0700

    create label and button and changes the config, get the text
    create label and button and changes the config, get the text

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
 import tkinter
+from tkinter import ttk
 
 root = tkinter.Tk()
-tkinter.Label(root, text="hello world").pack()
+button = tkinter.ttk.Button(root, text="Click me")
+button.pack()
+
+print( button["text"] ) #Click me
+
+button.config(text = "Push me") #text becomes Push me
+print( button.config() ) #{'class': ('class', '', '', '', ''), 'cursor': ('cursor', 'cursor', 'Cursor', '', ''), 'command': ('command', 'command', 'Command', '', ''), 'underline': ('underline', 'underline', 'Underline', -1, -1), 'compound': ('compound', 'compound', 'Compound', <index object: 'none'>, <index object: 'none'>), 'width': ('width', 'width', 'Width', '', ''), 'state': ('state', 'state', 'State', <index object: 'normal'>, <index object: 'normal'>), 'padding': ('padding', 'padding', 'Pad', '', ''), 'default': ('default', 'default', 'Default', <index object: 'normal'>, <index object: 'normal'>), 'takefocus': ('takefocus', 'takeFocus', 'TakeFocus', 'ttk::takefocus', 'ttk::takefocus'), 'style': ('style', 'style', 'Style', '', ''), 'textvariable': ('textvariable', 'textVariable', 'Variable', '', ''), 'text': ('text', 'text', 'Text', '', 'Push me'), 'image': ('image', 'image', 'Image', '', '')}
+
+label = tkinter.ttk.Label(root, text="Hi all")
+label.pack()
+
 root.mainloop()
 
