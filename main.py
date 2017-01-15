604de8a63dfb391eeca793167ff61363e0f53bac
Raymond Seger <raymondseger@live.com>
2017-01-15 11:33:00 +0700

    progress bar and scale
    progress bar and scale

--- main.py
+++ main.py
@@ -6,17 +6,21 @@ from tkinter import ttk
 class HelloApp:
 
     def __init__(self, master):
-        #create select button
-        self.month = StringVar()
-        self.combobox = ttk.Combobox(master, textvariable=self.month)
-        self.combobox.pack()
-        self.combobox.config(values=("Jan", "feb", "mar"))
-        # set default value
-        self.combobox.set("Jan")
+        self.progressbar = ttk.Progressbar(master, orient=HORIZONTAL, length=200)
+        self.progressbar.pack()
+        self.progressbar.config(mode='indeterminate')
+        self.progressbar.start()
+        #self.progressbar.stop()
 
-        self.year = StringVar()
-        self.spinbox = Spinbox(master, from_=1990, to=2017, textvariable=self.year)
-        self.spinbox.pack()
+        self.progressbar2 = ttk.Progressbar(master, orient=HORIZONTAL, length=200)
+        self.progressbar2.pack()
+        self.progressbar2.config(mode='determinate', maximum=11.0, value=4.2)
+
+        # create scale
+        value = DoubleVar()
+        self.scale = ttk.Scale(master, orient=HORIZONTAL, length=400, variable=value, from_=0.0, to=11.0)
+        self.scale.pack()
+        self.scale.set(11.0)
 
 def main():
     root = Tk()
