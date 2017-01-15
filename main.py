c54fa9ef2216078f2836ab35a7a806f3a22b30a1
Raymond Seger <raymondseger@live.com>
2017-01-15 12:30:02 +0700

    ttk.Style()

--- main.py
+++ main.py
@@ -6,10 +6,38 @@ from tkinter import ttk
 class HelloApp:
 
     def __init__(self, master):
-        self.canvas = Canvas(master)
-        self.canvas.pack()
-        self.canvas.config(width=640, height=480)
-        self.canvas.create_line(160, 360, 480, 120, fill="blue", width=5)
+        self.button1 = ttk.Button(master, text="Button 1")
+        self.button1.pack()
+        self.button2 = ttk.Button(master, text="Button 2")
+        self.button2.pack()
+
+        self.style = ttk.Style()
+
+        # ('winnative', 'clam', 'alt', 'default', 'classic', 'vista', 'xpnative')
+        print(self.style.theme_names())
+
+        self.style.theme_use('winnative')
+
+        # set defaults for TTK
+        print( self.button1.winfo_class() )  #TButton
+        self.style.configure('TButton', foreground="blue")
+
+        # create "CSS class" and use it for a button
+        self.style.configure('Alarm.TButton', foreground="orange", font=("Arial", 24, 'bold'))
+        self.button2.config(style="Alarm.TButton")
+
+        # add style for pressed state / disabled state
+        self.style.map("Alarm.TButton", foreground=[('pressed', 'pink'), ('disabled', 'grey')])
+        # self.button2.state(['disabled'])
+
+        # get all the states
+        """
+        [('Button.border',
+          {'children': [('Button.padding', {'children': [('Button.label', {'sticky': 'nswe'})], 'sticky': 'nswe'})],
+           'sticky': 'nswe'})]
+        """
+        print( self.style.layout('TButton') )
+
 
 def main():
     root = Tk()
