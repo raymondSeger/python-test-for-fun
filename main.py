2df417b9ff47ea83262dde78bee5846f176f4cd7
Raymond Seger <raymondseger@live.com>
2017-01-15 12:02:26 +0700

    Text ttk

--- main.py
+++ main.py
@@ -6,25 +6,8 @@ from tkinter import ttk
 class HelloApp:
 
     def __init__(self, master):
-        self.notebook = ttk.Notebook(master)
-        self.notebook.pack()
-
-        self.frame1 = ttk.Frame(self.notebook)
-        self.frame2 = ttk.Frame(self.notebook)
-
-        self.notebook.add(self.frame1, text="One")
-        self.notebook.add(self.frame2, text="Two")
-
-        ttk.Button(self.frame1, text="Click Me").pack()
-
-        # add more frame
-        self.frame3 = ttk.Frame(self.notebook)
-        self.notebook.insert(1, self.frame3, text="Three") #first param is position
-
-        self.notebook.tab(1, state="disabled")
-        #self.notebook.tab(1, state="hidden")
-
-
+        self.text = Text(master, width=40, height=10)
+        self.text.pack()
 
 def main():
     root = Tk()
