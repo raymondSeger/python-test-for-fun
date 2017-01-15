99eeeb77aa549dd8fa25fa8d7f798a2ad47bb9dd
Raymond Seger <raymondseger@live.com>
2017-01-15 10:44:04 +0700

    add picture to label, wrap text, text-align it, use font, etc
    add picture to label, wrap text, text-align it, use font, etc

--- main.py
+++ main.py
@@ -7,12 +7,20 @@ class HelloApp:
 
     def __init__(self, master):
 
-        self.label = ttk.Label(master, text = "Hello, Tkinter!")
-        self.label.grid(row = 0, column = 0, columnspan = 2)
+        self.label = ttk.Label(master, text = "Hello, Tkinter!").pack()
 
-        ttk.Button(master, text = "Texas", command = self.texas_hello).grid(row = 1, column = 0)
+        ttk.Button(master, text = "Texas", command = self.texas_hello).pack()
 
-        ttk.Button(master, text = "Hawaii", command = self.hawaii_hello).grid(row = 1, column = 1)
+        ttk.Button(master, text = "Hawaii", command = self.hawaii_hello).pack()
+
+        self.label2 = ttk.Label(master, text="Hawaii HawaiiHawaii HawaiiHawaiiHawaiiHawaiiHawaiiHawaiiHawaiiHawaiiHawaiiHawaiiHawaiiHawaii")
+        self.label2.config(wraplength=150) # 150 width/chars wrapping
+        self.label2.config(justify=CENTER) #textalign center
+        self.label2.config(foreground='blue', background='yellow')
+        self.label2.img = PhotoImage(file='a634dc.png')
+        self.label2.config(image=self.label2.img, compound='left')
+
+        self.label2.pack()
 
     #changes the text when button clicked
     def texas_hello(self):
