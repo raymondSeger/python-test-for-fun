509f486ae79680265a83e52a4bd3dd76e7475b70
Raymond Seger <raymondseger@live.com>
2017-01-15 11:24:32 +0700

    text field

--- main.py
+++ main.py
@@ -6,30 +6,13 @@ from tkinter import ttk
 class HelloApp:
 
     def __init__(self, master):
-        #create checkbox
-        self.checkbutton = ttk.Checkbutton(master, text="SPAM?")
-        self.checkbutton.pack()
+        # create text field / entry
+        self.entry = ttk.Entry(master, width=30)
+        self.entry.pack()
 
-        self.checkbutton2 = ttk.Checkbutton(master, text="SPAM?")
-        self.checkbutton2.pack()
-
-        self.radiobutton = ttk.Radiobutton(master, text="SPAM?")
-        self.radiobutton.pack()
-
-        self.radiobutton2 = ttk.Radiobutton(master, text="SPAM?")
-        self.radiobutton2.pack()
-
-        string_var = StringVar()
-        string_var.set("SPAM!")
-        print( string_var.get() ) #SPAM!
-
-        # when we check one of the checkbox, both will be the same because both use the same StringVar.
-        self.checkbutton.config(variable=string_var, onvalue="SPAM PLEASE", offvalue="BOO SPAM")
-        self.checkbutton2.config(variable=string_var, onvalue="SPAM PLEASE", offvalue="BOO SPAM")
-
-        # when we check one of the radiobutton, both will be the same because both use the same StringVar.
-        self.radiobutton.config(variable=string_var, )
-        self.radiobutton2.config(variable=string_var,)
+        self.entry.insert(0, "Enter your password")
+        #everything you type becomes *
+        # self.entry.config(show="*")
 
 
 def main():
